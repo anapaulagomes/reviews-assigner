d05aa75a1f5ed119623cca5427f169c476007e47
Ana Gomes <agomes@thoughtworks.com>
2017-02-20 23:34:22 -0300

    Add fixture as setup to eliminate duplicated code

--- tests/test_reviewsapi.py
+++ tests/test_reviewsapi.py
@@ -5,42 +5,38 @@ import pytest
 import requests
 
 
+@pytest.fixture()
+def reviewsapi():
+    yield hunter.ReviewsAPI()
+
 @mock.patch('hunter.reviewsapi.requests.get')
-def test_retrieve_certifications_list(mock_certifications):
+def test_retrieve_certifications_list(mock_certifications, reviewsapi):
     projects_list = [{'project_id': 15, 'status': 'certified'},
                     {'project_id': 14, 'status': 'certified'}]
 
     mock_certifications.return_value.json.return_value = projects_list
-
     expected_certifications_list = [15, 14]
-
-    reviewsapi = hunter.ReviewsAPI()
     certifications_list = reviewsapi.certifications()
 
     assert certifications_list == expected_certifications_list
 
 
 @mock.patch('hunter.reviewsapi.requests.get')
-def test_retrieve_empty_certifications_list(mock_certifications):
+def test_retrieve_empty_certifications_list(mock_certifications, reviewsapi):
     mock_certifications.return_value.json.return_value = []
-
-    reviewsapi = hunter.ReviewsAPI()
     certifications_list = reviewsapi.certifications()
 
     assert certifications_list == []
 
 
 @mock.patch('hunter.reviewsapi.requests.get')
-def test_retrieve_certification_list_searching_just_for_certified_projects(mock_certifications):
+def test_retrieve_certification_list_searching_just_for_certified_projects(mock_certifications, reviewsapi):
     projects_list = [{'project_id': 145, 'status': 'certified'},
                     {'project_id': 15, 'status': 'applied'},
                     {'project_id': 14, 'status': 'certified'}]
 
     mock_certifications.return_value.json.return_value = projects_list
-
     expected_certifications_list = [145, 14]
-
-    reviewsapi = hunter.ReviewsAPI()
     certifications_list = reviewsapi.certifications()
 
     assert certifications_list == expected_certifications_list
@@ -48,23 +44,18 @@ def test_retrieve_certification_list_searching_just_for_certified_projects(mock_
 
 @mock.patch('hunter.UnauthorizedToken')
 @mock.patch('hunter.reviewsapi.requests.get')
-def test_unauthorized_url_access_when_try_access_to_certifications_list(mock_certifications, mock_http_error_handler):
+def test_unauthorized_url_access_when_try_access_to_certifications_list(mock_certifications, mock_http_error_handler, reviewsapi):
     mock_certifications.return_value.json.side_effect = requests.exceptions.HTTPError()
     mock_http_error_handler.side_effect = UnauthorizedToken()
-
-    reviewsapi = hunter.ReviewsAPI()
 
     with pytest.raises(UnauthorizedToken):
         reviewsapi.certifications()
 
 
 @mock.patch('hunter.reviewsapi.requests.post')
-def test_create_new_request_with_wanted_projects(mock_request):
+def test_create_new_request_with_wanted_projects(mock_request, reviewsapi):
     mock_request.status_code = 200
-
     fake_certifications_list = [42, 57]
-
-    reviewsapi = hunter.ReviewsAPI()
     response = reviewsapi.request_reviews(fake_certifications_list)
 
     assert response.status_code is not None
