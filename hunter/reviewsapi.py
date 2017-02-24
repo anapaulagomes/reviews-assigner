9c8a214452f593dddb38d3da5c7c425988159eff
Ana Gomes <agomes@thoughtworks.com>
2017-02-24 20:22:18 -0300

    Refactoring classes to isolate requests responsability from certification method

--- hunter/reviewsapi.py
+++ hunter/reviewsapi.py
@@ -13,17 +13,24 @@ class ReviewsAPI:
         token = os.environ['UDACITY_AUTH_TOKEN']
         self.headers = {'Authorization': token, 'Content-Length': '0'}
 
-    def certifications(self):
+    def go_get_them(self, request):
         try:
-            raw_response = requests.get(CERTIFICATIONS_URL, headers=self.headers)
+            raw_response = request()
             response = raw_response.json()
 
             raw_response.raise_for_status()
 
-            certifications_list = [item['project_id'] for item in response if item['status'] == 'certified']
-            return certifications_list
+            return response
         except requests.exceptions.HTTPError:
             raise UnauthorizedToken('Maybe it\'s time to change you token!')
+
+    def request_certifications(self):
+        return requests.get(CERTIFICATIONS_URL, headers=self.headers)
+
+    def certifications(self):
+        response = self.go_get_them(self.request_certifications)
+        return [item['project_id'] for item in response if item['status'] == 'certified']
+
 
     def certified_languages(self):
         try:
