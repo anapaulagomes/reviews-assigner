cac25ffedad60bad318867c6184c8fbcdcd7200b
Ana Paula Gomes <apgomes88@gmail.com>
2017-04-29 16:17:19 -0300

    Add refresh url

--- revas/endpoints.py
+++ revas/endpoints.py
@@ -4,3 +4,4 @@ NEW_SUBMISSION_REQUESTS = '{}/submission_requests.json'.format(BASE_URL)
 SUBMISSION_REQUESTS = '{}/me/submission_requests.json'.format(BASE_URL)
 REVIEWER = '{}/me'.format(BASE_URL)
 ASSIGNED_COUNT = '{}/me/submissions/assigned_count.json'.format(BASE_URL)
+REQUEST_REFRESH = '{}/submission_requests/{}/refresh.json'
